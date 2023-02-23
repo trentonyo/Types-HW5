2e1e7cd9441ef530620451cb784d27fa1c1307e5
Trenton Young <trentonyo@gmail.com>
2023-02-22 18:23:39 -0800

    Cleaned up ugly comments

--- trentonHW4/Trenton_HW5sol.hs
+++ trentonHW4/Trenton_HW5sol.hs
@@ -50,13 +50,9 @@ semCmd (IFELSE t f) ((B x):xs) = run (if x then t else f) xs
 semCmd (IFELSE t f) _ = RankError
 
 run :: Prog -> Stack -> Result
-run []            x       = A x                                -- Once we have run out of cmds, return the remaining stack
---run ((LDI a):xs)  x       = run xs ((I a):x)                  -- For LD integer, append the value to the stack                        -- !!Why aren't these in semCmd?
---run ((LDB a):xs)  x       = run xs ((B a):x)                                                                                          -- !!Why aren't these in semCmd?
---run ((DUP):xs)    x       = if (null x)   then RankError                            -- If the stack has fewer than one element, err   -- !!Why aren't these in semCmd?
---                                          else run xs ((head x):x)                -- Append the head of the stack to it               -- !!Why aren't these in semCmd?
+run [] x     = A x                        -- Once we have run out of cmds, return the remaining stack
 run (c:cs) s = case semCmd c s of
-                  RankError -> RankError      -- If calling secCmd c s results in a RankError, we return a RankError
-                  A s' -> run cs s'           -- If calling secCmd c s results in A (Result Stack) then recursively call run with the remaining cmd's on the Result Stack
-run _             []      = RankError                               -- An empty stack after all that? Errrrrrr
+                  RankError -> RankError  -- If calling secCmd c s results in a RankError, we return a RankError
+                  A s' -> run cs s'       -- If calling secCmd c s results in A (Result Stack) then recursively call run with the remaining cmd's on the Result Stack
+run _ []     = RankError                  -- An empty stack after all that? Errrrrrr
 
